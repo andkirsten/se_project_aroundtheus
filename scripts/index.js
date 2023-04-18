d00e7edc680af1442cc3e00f4fed70a316150040
andkirsten <and.kirsten@gmail.com>
2023-04-18 09:22:56 +0900

    index.js added

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+initalCards = [
+  {
+    name: "Myeongdong",
+    link: URL("https://unsplash.com/photos/Erld-XTqXv0"),
+  },
+  {
+    name: "Gyeongbukgong Palace",
+    link: URL("https://unsplash.com/photos/dNtwZ-VnZ30"),
+  },
+  {
+    name: "Namsan Tower",
+    link: URL("https://unsplash.com/photos/ku9Ftte6Ymo"),
+  },
+  {
+    name: "Dongdaemun Design Plaza",
+    link: URL("https://unsplash.com/photos/W4qdDbv8QUM"),
+  },
+  {
+    name: "Han River",
+    link: URL("https://unsplash.com/photos/WeWfpnynP9Y"),
+  },
+  {
+    name: "Bukhansan National Park",
+    link: URL("https://unsplash.com/photos/ciyW1NqHLj8"),
+  },
+  {
+    name: "Busan Book Street",
+    link: URL("https://unsplash.com/photos/olLVE0KlMyM"),
+  },
+  {
+    name: "Manjanggul Cave",
+    link: URL("https://unsplash.com/photos/arFD2OAAdrQ"),
+  },
+  {
+    name: "Starfield Coex Mall",
+    link: URL("https://unsplash.com/photos/jpeTs-VfP68"),
+  },
+];
