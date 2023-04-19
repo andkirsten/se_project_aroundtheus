a0247a9665b16bc90a82b7fba283750f0669759f
andkirsten <and.kirsten@gmail.com>
2023-04-19 21:23:14 +0900

    stage 3 added modal js

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,53 @@
 var initialCards = [
   {
     name: "Myeongdong",
-    link: URL("https://unsplash.com/photos/Erld-XTqXv0"),
+    link: "https://unsplash.com/photos/Erld-XTqXv0",
   },
   {
     name: "Gyeongbukgong Palace",
-    link: URL("https://unsplash.com/photos/dNtwZ-VnZ30"),
+    link: "https://unsplash.com/photos/dNtwZ-VnZ30",
   },
   {
     name: "Namsan Tower",
-    link: URL("https://unsplash.com/photos/ku9Ftte6Ymo"),
+    link: "https://unsplash.com/photos/ku9Ftte6Ymo",
   },
   {
     name: "Dongdaemun Design Plaza",
-    link: URL("https://unsplash.com/photos/W4qdDbv8QUM"),
+    link: "https://unsplash.com/photos/W4qdDbv8QUM",
   },
   {
     name: "Han River",
-    link: URL("https://unsplash.com/photos/WeWfpnynP9Y"),
+    link: "https://unsplash.com/photos/WeWfpnynP9Y",
   },
   {
     name: "Bukhansan National Park",
-    link: URL("https://unsplash.com/photos/ciyW1NqHLj8"),
+    link: "https://unsplash.com/photos/ciyW1NqHLj8",
   },
   {
     name: "Busan Book Street",
-    link: URL("https://unsplash.com/photos/olLVE0KlMyM"),
+    link: "https://unsplash.com/photos/olLVE0KlMyM",
   },
   {
     name: "Manjanggul Cave",
-    link: URL("https://unsplash.com/photos/arFD2OAAdrQ"),
+    link: "https://unsplash.com/photos/arFD2OAAdrQ",
   },
   {
     name: "Starfield Coex Mall",
-    link: URL("https://unsplash.com/photos/jpeTs-VfP68"),
+    link: "https://unsplash.com/photos/jpeTs-VfP68",
   },
 ];
+
+// Elements
+const profileEditBtn = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditCloseBtn = document.querySelector(
+  "#profile-edit-close-button"
+);
+
+profileEditBtn.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+
+profileEditCloseBtn.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
